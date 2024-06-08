93a302a7f508f78bff04884674f91ee7036cc7dd
Charles Enright <c.enright@me.com>
2024-06-08 11:04:49 +0000

    Add layer control to toggle between OpenStreetMap and LiDAR Data layers

--- static/js/lidar-map.js
+++ static/js/lidar-map.js
@@ -1,25 +1,36 @@
 var map = L.map('mapid').setView([52.4814, -3.9797], 8);
 
-// Adding the WMS layer for LiDAR DSM (hillshade) data
-var wmsLayer = L.tileLayer.wms("https://datamap.gov.wales/geoserver/ows", { // Base URL to the OWS endpoint
-layers: 'geonode:wales_lidar_dsm_1m_hillshade_cog',
-}).addTo(map);
-
-/*
 // Base Map Layer: OpenStreetMap
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-*/
+
+
+// Adding the WMS layer for LiDAR DSM (hillshade) data
+var wmsLayer = L.tileLayer.wms("https://datamap.gov.wales/geoserver/ows", { // Base URL to the OWS endpoint
+    layers: 'geonode:wales_lidar_dsm_1m_hillshade_cog',
+});
 
  // Marker at Swansea
  var marker = L.marker([51.62144, -3.943645]).addTo(map);
  marker.bindPopup("<b>Hello Swansea!</b>").openPopup();
 
-// Function to show location map clicked
-function onMapClick(e) {
+ // Function to show location map clicked
+ function onMapClick(e) {
     alert("You clicked the map at " + e.latlng);
 }
 
 map.on('click', onMapClick);
+
+
+// Layer control to toggle between layers
+var baseMaps = {
+     "OpenStreetMap": openStreetMap
+ };
+
+var overlayMaps = {
+     "LiDAR Data": wmsLayer
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
