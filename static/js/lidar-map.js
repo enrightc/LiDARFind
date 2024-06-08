f2528a326304504cada19ad59c4c6d093ffbf10d
Charles Enright <c.enright@me.com>
2024-06-08 10:53:48 +0000

    Add LiDAR DSM hillshade layer to map

--- static/js/lidar-map.js
+++ static/js/lidar-map.js
@@ -1,6 +1,18 @@
-var map = L.map('mapid').setView([52.4814, -3.9797], 7);
+var map = L.map('mapid').setView([52.4814, -3.9797], 8);
 
+// Adding the WMS layer for LiDAR DSM (hillshade) data
+var wmsLayer = L.tileLayer.wms("https://datamap.gov.wales/geoserver/ows", { // Base URL to the OWS endpoint
+layers: 'geonode:wales_lidar_dsm_1m_hillshade_cog',
+}).addTo(map);
+
+/*
+// Base Map Layer: OpenStreetMap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+*/
+
+ // Marker at Swansea
+ var marker = L.marker([51.62144, -3.943645]).addTo(map);
+ marker.bindPopup("<b>Hello Swansea!</b>").openPopup();
