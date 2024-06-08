38cacec47044def68a5f4396c4c4cf4f440f7eaf
Charles Enright <c.enright@me.com>
2024-06-08 10:43:40 +0000

    Initial leaflet map setup and display

---
+++ static/js/lidar-map.js
@@ -0,0 +1,6 @@
+var map = L.map('mapid').setView([52.4814, -3.9797], 7);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
