fdc87c960d98569e9583627db0258b5cea3dac5f
Charles Enright <c.enright@me.com>
2024-06-09 17:46:15 +0000

    Add click function to zoom to marker when clicked

--- static/js/user-profile-map.js
+++ static/js/user-profile-map.js
@@ -7,7 +7,7 @@ var openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png'
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Bing Maps Satellite Layer
+// Bing Maps Satellite Layer (commented out)
 /*
 var bingSatellite = new L.BingLayer('AjH7Kmd8nydYW5bYUgAmdOD0g7hZzlMdu5tlFLvVT8oCT-n-CeUQLRutNJJXLhpY', {
     type: 'Aerial'  // Use 'AerialWithLabels' if you want satellite images with labels
@@ -17,16 +17,34 @@ var bingSatellite = new L.BingLayer('AjH7Kmd8nydYW5bYUgAmdOD0g7hZzlMdu5tlFLvVT8o
 // Adding the WMS layer for LiDAR DSM (hillshade) data
 var wmsLayer = L.tileLayer.wms("https://datamap.gov.wales/geoserver/ows", { // Base URL to the OWS endpoint
     layers: 'geonode:wales_lidar_dsm_1m_hillshade_cog',
-});
+})
 
-// Make a request to the Flask route to get the user's site locations and display markers on map
-// Adapted from https://github.com/isntlee/Sagacity/blob/master/app.py
+// Fetch user locations and add markers to the map
+// Adapted from: https://github.com/isntlee/Sagacity/blob/master/templates/home.html
 fetch("/fetch_user_locations")
     .then(response => response.json())
     .then(data => {
         // Iterate through the location data and add markers to the map
         data.forEach(location => {
             const [lat, lng] = location.split(',').map(parseFloat);
-            L.marker([lat, lng]).addTo(map);
+            const marker = L.marker([lat, lng]).addTo(map);
+
+            // Add click event listener to each marker
+            marker.on('click', function() {
+                map.setView(marker.getLatLng(), 14); // Zoom to level 14 and center on the marker
+            });
         });
-    })
+    });
+
+
+// Layer control to toggle between layers
+var baseMaps = {
+    "OpenStreetMap": openStreetMap,
+    "Bing Satellite": bingSatellite
+};
+
+var overlayMaps = {
+    "LiDAR Data": wmsLayer
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
