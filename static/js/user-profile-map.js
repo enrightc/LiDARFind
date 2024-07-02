2210928b3fedfc1d02d5896d6cab665b4d0da812
Charles Enright <c.enright@me.com>
2024-07-02 17:50:21 +0000

    Add content to index.html and footer to base.html

--- static/js/user-profile-map.js
+++ static/js/user-profile-map.js
@@ -1,7 +1,7 @@
 // Initialize the map
 var initialView = [52.4814, -3.9797];
 var initialZoom = 8;
-var map = L.map('user-profile-map').setView(initialView, initialZoom);
+var map = L.map('homePageMap').setView(initialView, initialZoom);
 
 // Base Map Layer: OpenStreetMap
 var openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,98 +9,139 @@ var openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png'
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Bing Maps Satellite Layer
+/*
+var bingSatellite = new L.BingLayer('AjH7Kmd8nydYW5bYUgAmdOD0g7hZzlMdu5tlFLvVT8oCT-n-CeUQLRutNJJXLhpY', {
+    type: 'Aerial'  // Use 'AerialWithLabels' if you want satellite images with labels
+});
+*/
+
 // Adding the WMS layer for LiDAR DSM (hillshade) data
 var wmsLayer = L.tileLayer.wms("https://datamap.gov.wales/geoserver/ows", {
     layers: 'geonode:wales_lidar_dsm_1m_hillshade_cog',
 });
 
-// Fetch user-specific records
+// Custom icon for current marker
+var crosshairIcon = L.icon({
+    iconUrl: 'static/images/crosshair.svg',
+    iconSize: [38, 95], // size of the icon
+    iconAnchor: [19, 48], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+// Variable to store the current marker
+let currentMarker;
+
+// Array to store markers with associated data for search filters (period, site type, monument type)
+let allMarkers = [];
+
+// Fetch all records and display them on the map
 fetch("/fetch_user_records")
     .then(response => response.json())
     .then(data => {
-        userRecords(data.user_records);
+        displayRecords(data.all_records);
     });
 
-    function userRecords(data) {
-        // Iterate through user-specific records and add markers to the map
-        data.forEach(record => {
-            const location = record.location;  // Assuming location is a string "lat,lng"
-            const [lat, lng] = location.split(',').map(parseFloat);
-            const marker = L.marker([lat, lng]).addTo(map);
-    
-            // Create popup content
-            var popupContent = `
-                <b>Title:</b> ${record.title}<br>
-                <b>Period:</b> ${record.period}<br>
-                <b>Monument Type:</b> ${record.monument_type}
-            `;
-            
-    
-            // Credit: https://gis.stackexchange.com/questions/31951/showing-popup-on-mouse-over-not-on-click-using-leaflet
-            marker.bindPopup("Popup content");
-            marker.on('mouseover', function (e) {
-                this.openPopup();
-            });
-            marker.on('mouseout', function (e) {
-                this.closePopup();
-            });
-    
-            // Bind the popup to the marker
-            marker.bindPopup(popupContent);
-    
-            // Add click event listener to each marker
-            marker.on('click', function() {
-                map.setView(marker.getLatLng(), 14); // Zoom to level 14 and center on the marker;
-                displayRecordDetails(record);
-                // Adapted from: https://www.w3schools.com/w3css/w3css_sidebar.asp
-                document.getElementById("main").style.marginLeft = "25%";
-                document.getElementById("mySidebar").style.width = "25%";
-                document.getElementById("mySidebar").style.display = "block";
-                document.getElementById("openNav").style.display = 'none';
-                // Adjust for mobile
-                if (window.innerWidth <= 600) {
-                    document.getElementById("main").style.marginLeft = "50%";
-                    document.getElementById("mySidebar").style.width = "50%";
-                }
-            });
+function displayRecords(data) {
+    data.forEach(record => {
+        const location = record.location;  // Assuming location is a string "lat,lng"
+        const [lat, lng] = location.split(',').map(parseFloat);
+        const marker = L.marker([lat, lng]).addTo(map);
+
+        // Create popup content
+        var popupContent = `
+            <b>Title:</b> ${record.title}<br>
+            <b>Site Type:</b> ${record.site_type}<br>
+            <b>Monument Type:</b> ${record.monument_type}<br>
+            <b>Interpretation Type:</b> ${record.interpretation}<br>
+            <b>Period:</b> ${record.period}<br>
+            <b>Created by:</b> ${record.created_by}<br>
+            <b>Created on:</b> ${record.created_on}<br>
+        `;
+
+        marker.bindPopup(popupContent);
+        // Store marker with its associated data
+        allMarkers.push({
+            marker,
+            period: record.period,
+            site_type: record.site_type,
+            monument_type: record.monument_type
         });
-    }
-    
 
-function displayRecordDetails(record) {
-    // Populate the site details display box with record data
-    $('#record-title').text(record.title);
-    $('#record-period').text(record.period);
-    $('#record-monument-type').text(record.monument_type);
-    $('#record-site-type').text(record.site_type);
-    $('#record-interpretation').text(record.interpretation);
-    $('#record-created-on').text(record.created_on);
-    $('#record-prn').text(record.prn);
-
-    // Set the href for the edit button
-    $('.edit-btn').attr('href', `/edit_record/${record._id}`);
-
-    // Set the href for the delete button
-    $('.delete-btn').attr('href', `/delete_record/${record._id}`);
+    });
 }
 
-// Reset map view to initial state
-L.control.resetView({
-    position: "topleft",
-    title: "Reset view",
-    latlng: L.latLng([52.4814, -3.9797]),
-    zoom: 8,
-}).addTo(map);
-
-// Add event listener to reset the map view from site details display box.
-$('#reset-map-view').on('click', function() {
-    map.setView(initialView, initialZoom);
+// Functions for filtering and displaying map markers based on user search parameters
+// Event Listeners for dropdown Filters
+document.getElementById('period-filter').addEventListener('change', function () {
+    applyFilters();
+});
+document.getElementById('site_type_filter').addEventListener('change', function () {
+    applyFilters();
+});
+document.getElementById('monument_type_filter').addEventListener('change', function () {
+    applyFilters();
 });
 
+document.getElementById('reset-filters-btn').addEventListener('click', function () {
+    // Reset the filter dropdowns to their default values
+    // Reset the filter dropdowns to their default values
+    document.getElementById('period-filter').selectedIndex = 0;
+    document.getElementById('site_type_filter').selectedIndex = 0;
+    document.getElementById('monument_type_filter').selectedIndex = 0;
+
+    // Re-apply filters to show all markers
+    applyFilters();
+});
+
+// Function to apply filters and display markers accordingly on the map
+function applyFilters() {
+    const selectedPeriod = document.getElementById('period-filter').value;
+    const selectedSiteType = document.getElementById('site_type_filter').value;
+    const selectedMonumentType = document.getElementById('monument_type_filter').value;
+
+    allMarkers.forEach(item => {
+        let matchesPeriod = selectedPeriod === "" || item.period === selectedPeriod;
+        let matchesSiteType = selectedSiteType === "" || item.site_type === selectedSiteType;
+        let matchesMonumentType = selectedMonumentType === "" || item.monument_type === selectedMonumentType;
+
+        if (matchesPeriod && matchesSiteType && matchesMonumentType) {
+            map.addLayer(item.marker); // Show marker
+        } else {
+            map.removeLayer(item.marker); // Hide marker
+        }
+    });
+}
+
+// Add click event listener to the map to place a new marker and show sidebar
+map.on('click', function (e) {
+    var coords = e.latlng;
+
+    // Check if there is an existing marker and remove it
+    if (currentMarker) {
+        map.removeLayer(currentMarker);
+    }
+
+    // Update the input field with the coordinates
+    document.getElementById('location').value = `${coords.lat.toFixed(5)}, ${coords.lng.toFixed(5)}`;
+    
+
+    // Make the location input field readonly after it has been filled
+    document.getElementById('location').classList.add('readonly');
+    
+    // Create a new marker at the clicked coordinates
+    currentMarker = L.marker(coords, {icon: crosshairIcon}).addTo(map);
+});
+
+// Base Maps
 var baseMaps = {
-    "OpenStreetMap": openStreetMap
+    "OpenStreetMap": openStreetMap,
+    /* uncomment if using 
+    "Bing Satellite": bingSatellite
+    */
 };
 
+// Overlay Maps
 var overlayMaps = {
     "LiDAR Data": wmsLayer
 };
